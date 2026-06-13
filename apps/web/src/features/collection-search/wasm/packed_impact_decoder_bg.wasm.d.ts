0c13c61fed83bc2146703dacbd52912af1e5b703
hicancan <hicancan@qq.com>
2026-06-13 22:27:25 +0800

    fix: vendor packed impact wasm for fast builds

---
+++ apps/web/src/features/collection-search/wasm/packed_impact_decoder_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_packedimpactretrievalsession_free: (a: number, b: number) => void;
+export const decode_packed_impact_stats: (a: number, b: number) => [number, number, number, number];
+export const decode_packed_impact_to_json: (a: number, b: number) => [number, number, number, number];
+export const packedimpactretrievalsession_apply_terms_utf8_stats_f64: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number, number, number];
+export const packedimpactretrievalsession_new: (a: number) => number;
+export const packedimpactretrievalsession_score_entries_f64: (a: number) => [number, number];
+export const packedimpactretrievalsession_stats_f64: (a: number) => [number, number];
+export const retrieve_packed_impact_topk_scores_utf8: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number, number, number];
+export const retrieve_packed_impact_topk_stats_utf8: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number, number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
